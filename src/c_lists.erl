d4e38d63aab89ba8bb8f18638b0e3caa7e7a97a2
talgat065 <talgat06585@gmail.com>
2020-05-08 20:35:31 +0600

    ex 3-3

--- src/c_lists.erl
+++ src/c_lists.erl
@@ -10,7 +10,7 @@
 -author("talgat065").
 
 %% API
--export([create/1, create_reverse/1]).
+-export([create/1, create_reverse/1, print_nums/1, print_odds/1]).
 
 %% Write a function that returns a list of the format [1,2,..,N-1,N].
 %% Example:
@@ -25,3 +25,24 @@ create(N, Acc) -> create(N - 1, [N | Acc]).
 %%reverse_create(3) ⇒ [3,2,1].
 create_reverse(0) -> [];
 create_reverse(N) -> [N | create_reverse(N - 1)].
+
+%% Write a function that prints out the
+%% integers between 1 and N.
+%% Hint: use io:format("Number:~p~n",[N]).
+print_nums(N) -> print_nums(1, N).
+
+print_nums(N, N) -> print(N);
+print_nums(M, N) ->
+  print(N),
+  print_nums(M + 1, N).
+
+%% Write a function that prints out the
+%% even integers between 1 and N.
+%% Hint: use guards.
+print_odds(N) -> print_odds(1, N).
+print_odds(M, N) when N div 2 /= 0 ->
+  print(N),
+  print_nums(M + 1, N);
+print_odds(M, N) -> print_nums(M + 1, N).
+
+print(N) -> io:format("Number:~p~n", [N]).
